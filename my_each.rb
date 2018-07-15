157fbefdc9d18b5c0d774257e5bf28d6305adcef
github username <github email address>
2018-07-15 19:10:50 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(salutations) # put argument(s) here
+  i = 0
+  while i < salutations.length
+    yield(salutations[i])
+    i += 1
+  end
+  salutations
 end
